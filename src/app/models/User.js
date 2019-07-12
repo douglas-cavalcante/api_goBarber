52a3a9586abefc73746b3fb9bf6a24e509cf31e2
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-12 00:26:05 -0300

    Adiciona geração do token jwt com sessão

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
-import { async } from 'rxjs/internal/scheduler/async';
 
 class User extends Model {
   static init(sequelize) {
@@ -22,6 +21,10 @@ class User extends Model {
       }
     });
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
