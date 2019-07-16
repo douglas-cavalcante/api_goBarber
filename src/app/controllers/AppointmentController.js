738c5acf594f86f42eab4f1caf04b85293c1756a
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-16 17:04:14 -0300

    chore: Adiciona validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -26,6 +27,34 @@ class AppointmentController {
         error: 'You can only create appointmnets with providers',
       });
     }
+
+    /* validacao da data escolhida */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Past date are not permitted',
+      });
+    }
+
+    /* validação da existência de agendamentos no mesmo horário */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Appointment date is not available',
+      });
+    }
+
+    /* cadastro de agendamento */
 
     const appointment = await Appointment.create({
       user_id: req.userId,
