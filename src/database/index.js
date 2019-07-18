f0d3a326e30adc148c3e5e3b2dcab3fc80846e91
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-18 13:10:36 -0300

    Adiciona conexao com mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,8 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointments from '../app/models/Appointment';
-
 import databaseConfig from '../config/database';
 
 const models = [User, File, Appointments];
@@ -10,6 +10,7 @@ const models = [User, File, Appointments];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -18,6 +19,16 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobaber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
